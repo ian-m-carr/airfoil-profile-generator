538ae7e4894234700ed7d1d489f7d6e3dd903079
imc <imc@dev>
2023-03-24 18:21:15 +0000

    initial version

---
+++ airfoil-profile-generator/__init__.py
@@ -0,0 +1,185 @@
+# Variable: bl_info
+# Contains informations for Blender to recognize and categorize the addon.
+bl_info = {
+    "name": "Generate an airfoil profile mesh",
+    "description": "Creates a mesh containing a single vertex path following a parameterized airfoil path",
+    "author": "I.M Carr",
+    "version": (1, 0, 0),
+    "blender": (3, 4, 1),
+    "category": "Object",
+}
+
+import bpy
+import bmesh
+from bpy_extras import object_utils
+from bpy.props import FloatProperty, IntProperty, EnumProperty
+from . import naca_5_mod
+
+
+def add_box(width, height, depth):
+    """
+    This function takes inputs and returns vertex and face arrays.
+    no actual mesh data creation is done here.
+    """
+
+    verts = [
+        (+1.0, +1.0, -1.0),
+        (+1.0, -1.0, -1.0),
+        (-1.0, -1.0, -1.0),
+        (-1.0, +1.0, -1.0),
+        (+1.0, +1.0, +1.0),
+        (+1.0, -1.0, +1.0),
+        (-1.0, -1.0, +1.0),
+        (-1.0, +1.0, +1.0),
+    ]
+
+    faces = [
+        (0, 1, 2, 3),
+        (4, 7, 6, 5),
+        (0, 4, 5, 1),
+        (1, 5, 6, 2),
+        (2, 6, 7, 3),
+        (4, 0, 3, 7),
+    ]
+
+    # apply size
+    for i, v in enumerate(verts):
+        verts[i] = v[0] * width, v[1] * depth, v[2] * height
+
+    return verts, faces
+
+
+class AddNACA5Modified(bpy.types.Operator, object_utils.AddObjectHelper):
+    """Add a NACA 5 digit (modified) parameterized airfoil mesh"""
+    bl_idname = "mesh.airfoil_add"
+    bl_label = "NACA 5 digit (modified) airfoil mesh"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    num_points: IntProperty(
+        name="Points per surface",
+        description="Number of points to generate per surface",
+        min=2, max=1000,
+        default=40
+    )
+    point_spacing: EnumProperty(
+        name="Point distribution algorithm",
+        description="How to distribute the generated points",
+        items=(
+            ("1", "Equal spacing", "", 1),
+            ("2", "Half Cosine With Smaller Increments Near Leading edge", "", 2),
+            ("3", "Half Cosine With Smaller Increments Near 1", "", 3),
+            ("4", "Full Cosine", "", 4)
+        ),
+        default=4
+    )
+    chord_length: FloatProperty(
+        name="Chord length",
+        description="The length of the airfoil chord to generate (meters)",
+        min=0.0, max=10,
+        default=1.0,
+    )
+    lift_coefficient: IntProperty(
+        name="NACA5 Digit 1",
+        description="Digit 1, Design lift coefficient * 20/3 (2 == 0.3 Cl)",
+        min=0, max=20,
+        default=2
+    )
+    max_camber_pos: IntProperty(
+        name="NACA5 Digit 2",
+        description="Digit 2, The position of maximum camber divided by .05 (3 == 0.15 or 15% of chord)",
+        min=0, max=20,
+        default=3
+    )
+    camber_curve_type: EnumProperty(
+        name="NACA5 Digit 3",
+        description="Digit 3, a single digit indicating whether the camber is simple (S = 0) or reflex (S = 1)",
+        items=(
+            ("0", "0: Simple", "", 0),
+            ("1", "1: Reflex", "", 1)
+        ),
+        default=0
+    )
+    max_thickness_pct: IntProperty(
+        name="NACA5 Digits 4/5",
+        description="Digits 4/5, the maximum thickness in percent of chord",
+        min=0, max=100,
+        default=18
+    )
+    lead_edge_radius: IntProperty(
+        name="NACA5 mod Digit 1",
+        description="Modifier Digit 1, leading edge radius [6:default, 0:sharp, >6:larger radius]",
+        min=0, max=10,
+        default=6
+    )
+    max_thickness_pos: IntProperty(
+        name="NACA5 mod Digit 2",
+        description="Modifier Digit 1, position in 1/10 of chord of maximum thickness (from leading edge) default 30%",
+        min=0, max=10,
+        default=3
+    )
+    lead_edge_droop: FloatProperty(
+        name="Leading Edge droop",
+        description="Leading edge droop max",
+        min=0.0, max=1.0,
+        default=0.0,
+    )
+    lead_edge_droop_pos: FloatProperty(
+        name="Leading Edge droop 0 distance",
+        description="Position in 1/10 of chord where leading edge droop becomes 0 (from leading edge)",
+        min=0.0, max=1.0,
+        default=0.0,
+    )
+
+    def execute(self, context):
+        # create the airfoil generator
+        af = naca_5_mod.NACA5Modified(self.num_points)
+
+        # and set it's spacing
+        af.set_coord_spacing(int(self.point_spacing))
+
+        # calculate the profile
+        af.naca_five_modified(self.lift_coefficient, self.max_camber_pos, int(self.camber_curve_type),
+                              self.max_thickness_pct, self.lead_edge_radius, self.max_thickness_pos,
+                              self.lead_edge_droop, self.lead_edge_droop_pos)
+
+        verts_loc, edges = af.get_profile_verts_and_edges(self.chord_length)
+
+        mesh = bpy.data.meshes.new("Airfoil")
+
+        bm = bmesh.new()
+
+        for v_co in verts_loc:
+            bm.verts.new(v_co)
+
+        bm.verts.ensure_lookup_table()
+
+        for edge in edges:
+            bm.edges.new((bm.verts[edge[0]], bm.verts[edge[1]]))
+
+        bm.to_mesh(mesh)
+        mesh.update()
+
+        # add the mesh as an object into the scene with this utility module
+        object_utils.object_data_add(context, mesh, operator=self)
+
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(AddNACA5Modified.bl_idname, icon='MESH_CUBE')
+
+
+def register():
+    bpy.utils.register_class(AddNACA5Modified)
+    bpy.types.VIEW3D_MT_mesh_add.append(menu_func)  # Adds the new operator to an existing menu.
+
+
+def unregister():
+    bpy.utils.unregister_class(AddNACA5Modified)
+    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+
+
+# This allows you to run the script directly from Blender's Text editor
+# to test the add-on without having to install it.
+if __name__ == "__main__":
+    register()
---
+++ airfoil-profile-generator/naca_5_mod.py
@@ -0,0 +1,176 @@
+import math
+
+
+class NACA5Modified:
+    NP = 0
+
+    XCC = []
+    XU = []
+    YU = []
+    XL = []
+    YL = []
+    YT = []
+    YC = []
+
+    YLED = []
+
+    DYC = []
+    XQ = [0.0, 0.0]
+    YQ = [0.0, 0.0]
+    ALP = [0.0] * 14
+
+    def __init__(self, num_points: int = 100):
+        self.NP = num_points
+        self.XCC = [0.0] * self.NP
+        self.XU = [0.0] * self.NP
+        self.YU = [0.0] * self.NP
+        self.XL = [0.0] * self.NP
+        self.YL = [0.0] * self.NP
+        self.YT = [0.0] * self.NP
+        self.YC = [0.0] * self.NP
+        self.DYC = [0.0] * self.NP
+
+        self.YLED = [0.0] * self.NP
+
+    def calc_theta(self, SVAL) -> float:
+        if SVAL == 0:
+            return 0
+        if SVAL == 1:
+            return math.pi / 2
+        if SVAL == -1:
+            return -math.pi / 2
+        return math.atan(SVAL / math.sqrt(1 - math.pow(SVAL, 2)))
+
+    def set_coord_spacing(self, spacing: int):
+        match spacing:
+            case 1:
+                DELTH = 1 / (self.NP - 1)
+                for I in range(self.NP):
+                    self.XCC[I] = DELTH * (I)
+            case 2:
+                DELTH = math.pi / 2 / (self.NP - 1)
+                for I in range(self.NP):
+                    self.XCC[I] = 1 - math.cos(DELTH * (I))
+            case 3:
+                DELTH = math.pi / 2 / (self.NP - 1)
+                for I in range(self.NP):
+                    self.XCC[I] = math.cos(math.pi / 2 - DELTH * (I))
+            case _:
+                # default to Full Cosine
+                DELTH = math.pi / (self.NP - 1)
+                for I in range(self.NP):
+                    self.XCC[I] = .5 - .5 * math.cos(DELTH * (I))
+
+    def naca_five_modified(self, LL: int, PP: int, QQ: int, TOC: int, IP: int, TT: int, LED: float, LEDD: int):
+        if QQ < 0 or QQ > 1:
+            raise Exception("third digit should be 0 (normal) or 1 (reflex) for camber line")
+
+        LC = LL / 10
+        PC = PP / 20
+        TC = TOC / 100
+        MC = PC / 2
+        TP = TT / 10
+        D1 = (2.24 - 5.42 * TP + 12.3 * math.pow(TP, 2)) / 10 / (1 - .878 * TP)
+        D2 = (.294 - 2 * (1 - TP) * D1) / math.pow((1 - TP), 2)
+        D3 = (-.196 + (1 - TP) * D1) / math.pow((1 - TP), 3)
+        A0 = .296904 * IP / 6
+        R1 = math.pow((1 - TP), 2) / 5 / (.588 - 2 * D1 * (1 - TP))
+        AA1 = .3 / TP - 15 * A0 / 8 / math.sqrt(TP) - TP / 10 / R1
+        A2 = -.3 / math.pow(TP, 2) + 5 * A0 / 4 / math.pow(TP, (1.5)) + 1 / 5 / R1
+        A3 = .1 / math.pow(TP, 3) - .375 * A0 / math.pow(TP, (2.5)) - 1 / 10 / R1 / TP
+
+        while (True):
+            PCT = math.sqrt(math.pow(MC, 2) * (MC / 3 - 2 * math.sqrt(MC / 3) + 1))
+            if abs(PC - PCT) <= .0001:
+                break
+
+            MC = MC * PC / PCT
+
+        SVAL = 1 - 2 * MC
+
+        THETA = self.calc_theta(SVAL)
+
+        QC = (3 * MC - 7 * math.pow(MC, 2) + 8 * math.pow(MC, 3) - 4 * math.pow(MC, 4)) / math.sqrt(MC * (1 - MC)) - 1.5 * (1 - 2 * MC) * (math.pi / 2 - THETA)
+        K1 = 9 * LC / QC
+        K2K1 = (3 * math.pow((MC - PC), 2) - math.pow(MC, 3)) / math.pow((1 - MC), 3)
+
+        for I in range(self.NP):
+            # thickness distribution (before/after max thickness position)
+            if self.XCC[I] > TP:
+                self.YT[I] = 5 * TC * (.002 + D1 * (1 - self.XCC[I]) + D2 * math.pow((1 - self.XCC[I]), 2) + D3 * math.pow((1 - self.XCC[I]), 3))
+            else:
+                self.YT[I] = 5 * TC * (A0 * math.sqrt(self.XCC[I]) + AA1 * self.XCC[I] + A2 * math.pow(self.XCC[I], 2) + A3 * math.pow(self.XCC[I], 3))
+
+            if LL == 0: continue  # LL is lift, 0 implies no lift, eg tail fin or strake
+
+            # camber distribution
+            if QQ == 0:  # normal camber curve
+                if self.XCC[I] > MC:  # Back
+                    self.YC[I] = (K1 / 6) * math.pow(MC, 3) * (1 - self.XCC[I])
+                    self.DYC[I] = (-K1 / 6) * math.pow(MC, 3)
+                else:  # Front
+                    self.YC[I] = (K1 / 6) * (math.pow(self.XCC[I], 3) - 3 * MC * math.pow(self.XCC[I], 2) + math.pow(MC, 2) * (3 - MC) * self.XCC[I])
+                    self.DYC[I] = (K1 / 6) * (3 * math.pow(self.XCC[I], 2) - 6 * MC * self.XCC[I] + math.pow(MC, 2) * (3 - MC))
+            else:  # reflex camber curve
+                if self.XCC[I] > MC:  # Back
+                    self.YC[I] = (K1 / 6) * (
+                            K2K1 * math.pow((self.XCC[I] - MC), 3) - K2K1 * math.pow((1 - MC), 3) * self.XCC[I] - math.pow(MC, 3) * self.XCC[I] + math.pow(
+                        MC, 3))
+                    self.DYC[I] = (K1 / 6) * (3 * K2K1 * math.pow((self.XCC[I] - MC), 2) - K2K1 * math.pow((1 - MC), 3) - math.pow(MC, 3))
+                else:  # Front
+                    self.YC[I] = (K1 / 6) * (
+                            math.pow((self.XCC[I] - MC), 3) - K2K1 * math.pow((1 - MC), 3) * self.XCC[I] - math.pow(MC, 3) * self.XCC[I] + math.pow(MC, 3))
+                    self.DYC[I] = (K1 / 6) * (3 * math.pow((self.XCC[I] - MC), 2) - K2K1 * math.pow((1 - MC), 3) - math.pow(MC, 3))
+
+        if LED > 0 and LEDD > 0:
+            for I in range(self.NP):
+                # do we have any droop to add to camber
+                if self.XCC[I] < LEDD / 10:
+                    self.YLED[I] = LED * math.pow(1 - (self.XCC[I] / (LEDD / 10)), 2)
+
+        # leading edge radius
+        self.LER = 1.1019 * math.pow((IP / 6 * TC), 2)
+
+        if IP >= 9:
+            self.LER = 3 * 1.1019 * math.pow((TC), 2)
+
+        # trailing edge angle
+        self.TEANG = 2 * math.atan(1.16925 * TC)
+
+        # designation
+        DESIG = LL * 10000 + PP * 1000 + QQ * 100 + TOC
+        self.DESIG_str = str(DESIG)
+
+        # modification designation
+        SESIG = IP * 10 + TT
+        self.DESIG_str = self.DESIG_str + "-" + str(SESIG)
+
+        # now derive the surfaces
+        for I in range(self.NP):
+            THET = 0  # math.atan(DYC[I])
+            self.XU[I] = self.XCC[I] - self.YT[I] * math.sin(THET)
+            self.YU[I] = self.YC[I] + self.YT[I] * math.cos(THET) - self.YLED[I]
+            self.XL[I] = self.XCC[I] + self.YT[I] * math.sin(THET)
+            self.YL[I] = self.YC[I] - self.YT[I] * math.cos(THET) - self.YLED[I]
+
+    def get_profile_verts_and_edges(self, chord_length: float) -> list[list[list[float]], list[list[int]]]:
+        # verts are in x/z plane with 0 y
+        verts = [] # list of lists of 3 floats [x,y,z]
+        edges = [] # list of lists of 2 vertex indices [1,2]
+
+        for I in range(self.NP - 1, -1, -1):
+            verts.append([self.XU[I] * chord_length, 0, self.YU[I] * chord_length])
+
+        # then forward along the lower
+        for I in range(self.NP):
+            verts.append([self.XL[I] * chord_length, 0, self.YL[I] * chord_length])
+
+        for I in range((self.NP *2) -1):
+            edges.append([I, I+1])
+
+        # finally close the path at the trailing edge
+        if self.YU[self.NP - 1] != self.YL[self.NP - 1]:
+            edges.append([(self.NP *2) -1, 0])
+
+
+        return [verts, edges]
